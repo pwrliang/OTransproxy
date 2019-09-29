e8502fca4d198491cb19de0ce5d7184648cfdddf
gengliang <gengliang@guazi.com>
2019-09-29 15:23:27 +0800

    Using md5sum instead of date as part of version file

--- rules/update.sh
+++ rules/update.sh
@@ -4,7 +4,7 @@ VERSION_FILE="$WORKPLACE/version"
 ACCELERATED_DOMAINS="$WORKPLACE/accelerated-domains.china.txt"
 CHNROUTE="$WORKPLACE/chnroute.txt"
 
-echo "# RULE  NUM  DATE" > "$VERSION_FILE"
+echo "# RULE  NUM  CHECKSUM" > "$VERSION_FILE"
 
 echo 'Generating "accelerated-domains.china"'
 if wget --no-check-certificate \
@@ -13,7 +13,7 @@ if wget --no-check-certificate \
         https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf \
         | sed '/^#/d' | cut -d "/" -f 2 | sort | uniq > "$ACCELERATED_DOMAINS"; then
   echo "Ok"
-  echo "ACCELERATED_DOMAINS $(wc -l < "$ACCELERATED_DOMAINS")    $(date)" >> "$VERSION_FILE"
+  echo "ACCELERATED_DOMAINS $(wc -l < "$ACCELERATED_DOMAINS")    $(md5sum "$ACCELERATED_DOMAINS")" >> "$VERSION_FILE"
 else
   echo "Error"
   rm "$ACCELERATED_DOMAINS" >/dev/null 2>&1
@@ -27,7 +27,7 @@ if wget --no-check-certificate \
         https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/ipip_country/ipip_country_cn.netset \
         | sed '/^#/d' | sort | uniq > "$CHNROUTE"; then
   echo "Ok"
-  echo "CHNROUTE $(wc -l < "$CHNROUTE")    $(date)" >> "$VERSION_FILE"
+  echo "CHNROUTE $(wc -l < "$CHNROUTE")    $(md5sum "$CHNROUTE")" >> "$VERSION_FILE"
 else
   echo "Error"
   rm "$CHNROUTE" >/dev/null 2>&1
