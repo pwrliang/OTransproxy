1f77f94b8335c6468c5b4965457c8e5d747e6568
gengliang <gengliang@guazi.com>
2019-10-07 17:50:03 +0800

    Improve: read host list from config files

--- otransproxy.sh
+++ otransproxy.sh
@@ -1,29 +1,76 @@
 #!/usr/bin/env sh
-WORKPLACE=$(dirname "$0")
-TMP=$WORKPLACE/tmp
-ACCELERATED_DOMAINS_CONF=$TMP/transproxy_accelerated_domains.conf
-ALLOW_LIST_CONF=$TMP/transproxy_allowlist.conf
-BLOCK_LIST_CONF=$TMP/transproxy_blocklist.conf
-DNS_SERVER_CONF=$TMP/transproxy_server.conf
-DNSMASQ_CONF=/tmp/dnsmasq.d
+WORKPLACE="$(cd "$(dirname "$0")" && pwd -P)"
+# shellcheck disable=SC2128
+SCRIPT_NAME=$(basename "$BASH_SOURCE")
+SCRIPT_PATH="$WORKPLACE/$SCRIPT_NAME"
+INIT_PATH="/etc/init.d/otransproxy"
+LOG="$WORKPLACE/log"
+TMP="$WORKPLACE/tmp"
+ACCELERATED_DOMAINS_CONF="$TMP/transproxy_accelerated_domains.conf"
+BYPASS_LIST_CONF="$TMP/transproxy_bypass_domains.conf"
+PROXY_LIST_CONF="$TMP/transproxy_proxy_domains.conf"
+DNS_SERVER_CONF="$TMP/transproxy_server.conf"
+DNSMASQ_CONF="/tmp/dnsmasq.d"
 UPSTREAM_DNS="192.168.1.1"
 if [ -f "/tmp/resolv.conf.auto" ]; then
   UPSTREAM_DNS=$(cut -d " " -f 2 </tmp/resolv.conf.auto|grep -v 0.0.0.0|grep -v 127.0.0.1|sed -n 2p)
 fi
 V2RAY_TPROXY_PORT=12345
 V2RAY_DNS_PORT=7913
-# These sites are V2Ray servers
-# We bypass websocket-based v2ray servers
-ALLOW_DOMAIN_LIST="ws.example.com ws.example1.com"
-# We bypass the following domains for SoftEther to get original IP
-ALLOW_DOMAIN_LIST="$ALLOW_DOMAIN_LIST softether-network.net softether.co.jp flets-east.jp flets-west.jp ddns.uxcom.jp"
-ALLOW_DOMAIN_LIST="$ALLOW_DOMAIN_LIST plex.tv plex.direct apple.com microsoft.com"
-ALLOW_IP_LIST="0.0.0.0/8 10.0.0.0/8 100.64.0.0/10 127.0.0.0/8 169.254.0.0/16 172.16.0.0/12 192.168.0.0/16 224.0.0.0/4 240.0.0.0/4 223.5.5.5 223.6.6.6 114.114.114.114 114.114.115.115 1.2.4.8 210.2.4.8 112.124.47.27 114.215.126.16 180.76.76.76 119.29.29.29"
-# These sites are forced using V2ray proxy
-BLOCK_IP_LIST=""
-BLOCK_DOMAIN_LIST="google.com.tw google.com.ncr github.com github.io raw.githubusercontent.com apnic.net s3.amazonaws.com openwrt.org"
+BYPASS_DOMAIN_LIST=""
+BYPASS_IP_LIST=""
+PROXY_DOMAIN_LIST=""
+PROXY_IP_LIST=""
 # UPSTREAM/FOREIGN
 DNS_PREFERENCE="UPSTREAM"
+
+# First param is path. Second param is boolean, 1 means return IPs otherwise return domains
+read_host_list() {
+  path=$1
+  return_ip=$2
+  ip_list=""
+  domain_list=""
+
+  # shellcheck disable=SC2039
+  while IFS= read -r line || [[ -n "${line}" ]]; do
+    line=$(echo "${line}" | xargs)
+    if [ -z "${line}" ] || [ "$(echo "${line}" | cut -c 1)" = "#" ]; then
+      continue
+    fi;
+
+    if [[ $line =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+(/[0-9]+)?$ ]]; then
+      ip_list="$line $ip_list"
+    else
+      domain_list="$line $domain_list"
+    fi
+  done < "$path"
+
+  if [ "$return_ip" -eq 1 ]; then
+    echo "$ip_list" | xargs
+  else
+    echo "$domain_list" | xargs
+  fi;
+}
+
+load_list_file() {
+  BYPASS_LIST_FILE="$WORKPLACE/bypass.list"
+  if [ -f "$BYPASS_LIST_FILE" ]; then
+    BYPASS_DOMAIN_LIST=$(read_host_list "$BYPASS_LIST_FILE" 0)
+    BYPASS_IP_LIST=$(read_host_list "$BYPASS_LIST_FILE" 1)
+  else
+    echo "Can not found $BYPASS_LIST_FILE"
+    exit 1
+  fi
+
+  PROXY_LIST_FILE="$WORKPLACE/proxy.list"
+  if [ -f "$PROXY_LIST_FILE" ]; then
+    PROXY_DOMAIN_LIST=$(read_host_list "$PROXY_LIST_FILE" 0)
+    PROXY_IP_LIST=$(read_host_list "$PROXY_LIST_FILE" 1)
+  else
+    echo "Can not found $PROXY_LIST_FILE"
+    exit 1
+  fi
+}
 
 echo_date() {
   echo "$(date) - $1"
@@ -54,10 +101,10 @@ install_v2ray() {
            --timeout=8 \
            -qO- \
            "https://github.com/v2ray/v2ray-core/releases/download/$latest_version/$filename" > "$TMP/v2ray.zip"; then
-    echo_date "Downloaded"
+    echo "Downloaded"
     [ -d "$TMP/v2ray" ] && rm -rf "$TMP/v2ray"
     mkdir -p "$TMP/v2ray"
-    echo_date "Unziping..."
+    echo "Unziping..."
 
     if unzip "$TMP/v2ray.zip" -d "$TMP/v2ray" >/dev/null; then
       [ ! -d "$WORKPLACE/v2ray" ] && mkdir -p "$WORKPLACE/v2ray"
@@ -68,13 +115,49 @@ install_v2ray() {
         mv -f "$TMP/v2ray/v2ctl_softfloat" "$WORKPLACE/v2ray/v2ctl"
         mv -f "$TMP/v2ray/v2ray_softfloat" "$WORKPLACE/v2ray/v2ray"
       fi
-      echo_date "V2Ray installed"
+      echo "V2Ray installed"
     else
-      echo_date "Failed to unzip"
+      echo "Failed to unzip"
     fi
   else
-    echo_date "Failed to download"
+    echo "Failed to download"
     exit 1
+  fi
+}
+
+setup_init_script() {
+  cat > $INIT_PATH <<-EOF
+#!/bin/sh /etc/rc.common
+# The script to start OTransproxy service
+# Copyright (C) 2007 OpenWrt.org
+
+START=99
+STOP=99
+
+start() {
+    $SCRIPT_PATH --start >> $LOG/otransparent.log
+}
+
+stop() {
+    $SCRIPT_PATH --stop >> $LOG/otransparent.log
+}
+EOF
+  # shellcheck disable=SC2181
+  if [ $? -eq 0 ]; then
+    chmod +x $INIT_PATH
+    $INIT_PATH enable
+  else
+    echo "Failed to write init script"
+    exit 1
+  fi
+}
+
+teardown_init_script() {
+  if [ -f $INIT_PATH ]; then
+    $INIT_PATH disable
+    rm -f $INIT_PATH
+  else
+    echo "Can not found $INIT_PATH"
   fi
 }
 
@@ -172,8 +255,8 @@ config_dnsmasq() {
 
   if [ -d $DNSMASQ_CONF ]; then
     cp -f "$ACCELERATED_DOMAINS_CONF" "$DNSMASQ_CONF/"
-    cp -f "$ALLOW_LIST_CONF" "$DNSMASQ_CONF/"
-    cp -f "$BLOCK_LIST_CONF" "$DNSMASQ_CONF/"
+    cp -f "$BYPASS_LIST_CONF" "$DNSMASQ_CONF/"
+    cp -f "$PROXY_LIST_CONF" "$DNSMASQ_CONF/"
     cp -f "$DNS_SERVER_CONF" "$DNSMASQ_CONF/"
   else
     echo_date "Can not found $DNSMASQ_CONF"
@@ -227,16 +310,16 @@ update_rules() {
     echo "$latest_versions" >"$TMP/version"
   fi
 
-  echo "# These domain will be resolved by local dns server" > "$BLOCK_LIST_CONF"
-  for domain in $BLOCK_DOMAIN_LIST; do
-    echo "$domain" | sed "s/^/server=&\/./g" | sed "s/$/\/127.0.0.1#$V2RAY_DNS_PORT/g" >> "$BLOCK_LIST_CONF"
-    echo "$domain" | sed "s/^/ipset=&\/./g" | sed "s/$/\/BLOCK_SET/g" >> "$BLOCK_LIST_CONF"
+  echo "# These domain will be resolved by local dns server" > "$PROXY_LIST_CONF"
+  for domain in $PROXY_DOMAIN_LIST; do
+    echo "$domain" | sed "s/^/server=&\/./g" | sed "s/$/\/127.0.0.1#$V2RAY_DNS_PORT/g" >> "$PROXY_LIST_CONF"
+    echo "$domain" | sed "s/^/ipset=&\/./g" | sed "s/$/\/PROXY_SET/g" >> "$PROXY_LIST_CONF"
   done
 
-  echo "# These domain will be resolved by $UPSTREAM_DNS" > "$ALLOW_LIST_CONF"
-  for domain in $ALLOW_DOMAIN_LIST; do
-    echo "$domain" | sed "s/^/server=&\/./g" | sed "s/$/\/$UPSTREAM_DNS/g" >> "$ALLOW_LIST_CONF"
-    echo "$domain" | sed "s/^/ipset=&\/./g" | sed "s/$/\/ALLOW_SET/g" >> "$ALLOW_LIST_CONF"
+  echo "# These domain will be resolved by $UPSTREAM_DNS" > "$BYPASS_LIST_CONF"
+  for domain in $BYPASS_DOMAIN_LIST; do
+    echo "$domain" | sed "s/^/server=&\/./g" | sed "s/$/\/$UPSTREAM_DNS/g" >> "$BYPASS_LIST_CONF"
+    echo "$domain" | sed "s/^/ipset=&\/./g" | sed "s/$/\/BYPASS_SET/g" >> "$BYPASS_LIST_CONF"
   done
 
   [ ! -f "$DNS_SERVER_CONF" ] && {
@@ -255,25 +338,25 @@ update_rules() {
 
 flush_nat(){
   iptables -t nat -F V2RAY >/dev/null 2>&1
-  ipset -F ALLOW_SET >/dev/null 2>&1
-  ipset -F BLOCK_SET >/dev/null 2>&1
+  ipset -F BYPASS_SET >/dev/null 2>&1
+  ipset -F PROXY_SET >/dev/null 2>&1
 }
 
 creat_ipset(){
   echo_date "Creating ipset"
-	ipset -! create ALLOW_SET nethash && ipset flush ALLOW_SET
-  ipset -! create BLOCK_SET nethash && ipset flush BLOCK_SET
+	ipset -! create BYPASS_SET nethash && ipset flush BYPASS_SET
+  ipset -! create PROXY_SET nethash && ipset flush PROXY_SET
 	# 中国IP加入白名单
-	sed -e "s/^/add ALLOW_SET &/g" "$TMP/chnroute.txt" | awk '{print $0} END{print "COMMIT"}' | ipset -R
+	sed -e "s/^/add BYPASS_SET &/g" "$TMP/chnroute.txt" | awk '{print $0} END{print "COMMIT"}' | ipset -R
 
-	for ip in $ALLOW_IP_LIST
+	for ip in $BYPASS_IP_LIST
 	do
-		ipset -! add ALLOW_SET "$ip" >/dev/null 2>&1
+		ipset -! add BYPASS_SET "$ip" >/dev/null 2>&1
 	done
 
-	for ip in $BLOCK_IP_LIST
+	for ip in $PROXY_IP_LIST
 	do
-		ipset -! add BLOCK_SET $ip >/dev/null 2>&1
+		ipset -! add PROXY_SET $ip >/dev/null 2>&1
 	done
 }
 
@@ -282,9 +365,9 @@ apply_nat_rules(){
   # 新建一个名为 V2RAY 的链
   iptables -t nat -N V2RAY >/dev/null 2>&1
   # 直连白名单网站
-  iptables -t nat -A V2RAY -p tcp -m set --match-set ALLOW_SET dst -j RETURN
+  iptables -t nat -A V2RAY -p tcp -m set --match-set BYPASS_SET dst -j RETURN
   # 黑名单走代理
-  iptables -t nat -A V2RAY -p tcp -m set --match-set BLOCK_SET dst -j REDIRECT --to-ports $V2RAY_TPROXY_PORT
+  iptables -t nat -A V2RAY -p tcp -m set --match-set PROXY_SET dst -j REDIRECT --to-ports $V2RAY_TPROXY_PORT
   # 直连 SO_MARK 为 0xff 的流量(0xff 是 16 进制数，数值上等同与上面配置的 255)，此规则目的是避免代理本机(网关)流量出现回环问题
   iptables -t nat -A V2RAY -p tcp -j RETURN -m mark --mark 0xff
   # 其余流量转发到 12345 端口（即 V2Ray）
@@ -317,8 +400,9 @@ stop_service() {
 }
 
 main() {
-  mkdir -p "$WORKPLACE" >/dev/null 2>&1
+  mkdir -p "$LOG" >/dev/null 2>&1
   mkdir -p "$TMP" >/dev/null 2>&1
+  load_list_file
   case $1 in
 
   "--install")
@@ -341,15 +425,26 @@ main() {
     config_dnsmasq
     restart_dnsmasq
     ;;
+  "--enable")
+    setup_init_script
+    ;;
+  "--disable")
+    teardown_init_script
+    ;;
   *)
-    echo "A script auto config transproxy."
+    echo "OTransproxy - A script to auto config transparent proxy on OpenWrt based router."
+    echo "Project page: https://github.com/pwrliang/OTransproxy"
+    echo ""
     echo "options:"
     echo "  --install: Install v2ray and download rules"
     echo "  --start: Start V2RAY and config transproxy"
     echo "  --stop: Stop V2RAY and flush transproxy"
     echo "  --restart: restart is an alise of --stop & --start"
     echo "  --update-rules: Download rules"
-    echo "  NOTE: You should put config.json under $WORKPLACE"
+    echo "  --enable: enable startup for OTransproxy when the system up"
+    echo "  --disable: disable startup for OTransproxy when the system up"
+    echo ""
+    echo "NOTE: You should put config.json, bypass.list and proxy.list under $WORKPLACE"
     ;;
   esac
 }
